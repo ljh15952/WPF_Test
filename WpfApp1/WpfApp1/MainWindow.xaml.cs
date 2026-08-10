412d792df12bb70ac7b645d34c9e9661a6455284
LeeJeungHwa <ljh15952@gmail.com>
2026-08-10 15:46:19 +0900

    Add files via upload

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -14,18 +14,12 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // Get the text from the TextBox
-            string inputText = txtName.Text;
-            txtResult.Text = $"Hello, {inputText}!";
-        }
     }
 }

0d7e49260e63ead11841c09fde7cb3a9bcb946a4
LeeJeungHwa <ljh15952@gmail.com>
2026-08-12 16:58:07 +0900

    Add Employee class with properties

---
+++ code/Employee.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+    }
+}
